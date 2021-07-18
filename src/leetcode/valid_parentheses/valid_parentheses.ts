09512c36920e7d21d17baad39b6178150052e195
Kegbi <kegbi.networking@gmail.com>
2021-07-18 22:19:18 +0300

    Added another solution for valid_parentheses task, which uses only one stack and is little bit faster, than the previous one

--- src/leetcode/valid_parentheses/valid_parentheses.ts
+++ src/leetcode/valid_parentheses/valid_parentheses.ts
@@ -1,27 +1,58 @@
+// First solution
+
+// export default function validParentheses(string: string): boolean {
+//   type OpeningBracket = "(" | "{" | "[";
+//   type ClosingBracket = ")" | "}" | "]";
+//
+//   const openBrackets = ["(", "{", "["];
+//   const closedBrackets = [")", "}", "]"];
+//   const openStack: OpeningBracket[] = [];
+//   const closedStack: ClosingBracket[] = [];
+//
+//   const splittedBrackets = string.split("");
+//   splittedBrackets.forEach((bracket: string) => {
+//     if (openBrackets.includes(bracket)) {
+//       openStack.push(bracket as OpeningBracket);
+//     } else {
+//       closedStack.push(bracket as ClosingBracket);
+//       const bracketIndex = closedBrackets.indexOf(bracket);
+//       if (openStack.length > 0) {
+//         if (openStack[openStack.length - 1] === openBrackets[bracketIndex]) {
+//           openStack.pop();
+//           closedStack.pop();
+//         }
+//       }
+//     }
+//   });
+//
+//   return openStack.length === 0 && closedStack.length === 0;
+// }
+
+// Second solution, uses only 1 stack
+
 export default function validParentheses(string: string): boolean {
   type OpeningBracket = "(" | "{" | "[";
   type ClosingBracket = ")" | "}" | "]";
 
-  const openBrackets = ["(", "{", "["];
-  const closedBrackets = [")", "}", "]"];
-  const openStack: OpeningBracket[] = [];
-  const closedStack: ClosingBracket[] = [];
+  const matching: { [key in OpeningBracket]: ClosingBracket } = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack: string[] = [];
 
-  const splittedBrackets = string.split("");
-  splittedBrackets.forEach((bracket: string) => {
-    if (openBrackets.includes(bracket)) {
-      openStack.push(bracket as OpeningBracket);
+  for (const char of string) {
+    // If current symbol is opening bracket - push it to the stack
+    if (matching[char]) {
+      stack.push(char);
+      // If current symbol !== closing bracket, associated with last symbol in stack - return false
+    } else if (char !== matching[stack[stack.length - 1]]) {
+      return false;
     } else {
-      closedStack.push(bracket as ClosingBracket);
-      const bracketIndex = closedBrackets.indexOf(bracket);
-      if (openStack.length > 0) {
-        if (openStack[openStack.length - 1] === openBrackets[bracketIndex]) {
-          openStack.pop();
-          closedStack.pop();
-        }
-      }
+      // In other cases - delete last element from stack
+      stack.pop();
     }
-  });
+  }
 
-  return openStack.length === 0 && closedStack.length === 0;
+  return stack.length === 0;
 }
