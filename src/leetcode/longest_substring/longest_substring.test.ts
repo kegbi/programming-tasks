0b4380ad0d89dd5527ff73410de7a6970eb0b8e6
Kegbi <kegbi.networking@gmail.com>
2021-07-20 00:07:13 +0300

    Ugly solution to first medium task

---
+++ src/leetcode/longest_substring/longest_substring.test.ts
@@ -0,0 +1,34 @@
+import lengthOfLongestSubstring from "./longest_substring";
+
+describe("longest substring test cases", function () {
+  test("First example", () => {
+    const input = "abcabcbb";
+    const output = 3;
+    expect(lengthOfLongestSubstring(input)).toEqual(output);
+  });
+  test("Second example", () => {
+    const input = "bbbbb";
+    const output = 1;
+    expect(lengthOfLongestSubstring(input)).toEqual(output);
+  });
+  test("Third example", () => {
+    const input = "pwwkew";
+    const output = 3;
+    expect(lengthOfLongestSubstring(input)).toEqual(output);
+  });
+  test("Forth example", () => {
+    const input = "";
+    const output = 0;
+    expect(lengthOfLongestSubstring(input)).toEqual(output);
+  });
+  test("Fifth example", () => {
+    const input = "aab";
+    const output = 2;
+    expect(lengthOfLongestSubstring(input)).toEqual(output);
+  });
+  test("Fifth example", () => {
+    const input = "dvdf";
+    const output = 3;
+    expect(lengthOfLongestSubstring(input)).toEqual(output);
+  });
+});
--- src/leetcode/two_sum/two_sum.test.ts
+++ src/leetcode/two_sum/two_sum.test.ts
@@ -1,5 +1,4 @@
-// const twoSum = require("./two_sum");
-import twoSum from "./two_sum"
+import twoSum from "./two_sum";
 
 describe("two sum case", function () {
   test("First example", () => {
@@ -9,13 +8,13 @@ describe("two sum case", function () {
     expect(twoSum(array, target)).toEqual(output);
   });
   test("Second example", () => {
-    const array = [3,2,4];
+    const array = [3, 2, 4];
     const target = 6;
     const output = [1, 2];
     expect(twoSum(array, target)).toEqual(output);
   });
   test("Third example", () => {
-    const array = [3,3];
+    const array = [3, 3];
     const target = 6;
     const output = [0, 1];
     expect(twoSum(array, target)).toEqual(output);
