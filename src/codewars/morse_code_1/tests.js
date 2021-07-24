69e6b8ed20acd89f5722ef69a875b21cf5f45cae
Kegbi <kegbi.networking@gmail.com>
2021-07-24 06:09:34 +0300

    Removed old and bad solutions for codewars website

--- src/codewars/morse_code_1/tests.js
+++
@@ -1,7 +0,0 @@
-Test.describe("Example from description", function(){
-    Test.assertEquals(decodeMorse('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
-    });
-    
-    Test.describe("Your own tests", function(){
-    // Add more tests here
-    });
--- src/codewars/replace_letter_with_alphabet_position/script2.js
+++
@@ -1,9 +0,0 @@
-// let str = 'abcdefghijklmnopqrstuvwxyz';
-// let result = str.split();
-// console.log(result);
-
-let str = [...Array(26)].reduce(a=>a+String.fromCharCode(i++),'',i=97);
-// console.log(str);
-
-// let array = [...Array(26)];
-console.log(str);
