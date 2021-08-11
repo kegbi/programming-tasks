818e6f9562ae20dc7a17326656a73c2734c54922
Kegbi <kegbi.networking@gmail.com>
2021-08-11 22:48:10 +0300

    Added solutions to 203 problem

---
+++ src/leetcode/Easy/203_remove_linked_list_elements/remove_linked_list_elements.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+// @ts-ignore
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  // @ts-ignore
+  let result = new ListNode(0);
+  result.next = head;
+
+  let current = result;
+
+  while (current.next) {
+    if (current.next.val === val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return result.next;
+}
