ea7a589742041329664807c99429b68517c97b30
Kegbi <kegbi.networking@gmail.com>
2021-09-12 21:47:33 +0300

    Prettified two_sum solution

--- src/websites/leetcode/Easy/1_two_sum/two_sum.ts
+++ src/websites/leetcode/Easy/1_two_sum/two_sum.ts
@@ -1,35 +1,18 @@
-// Brutforce solution
-
-// export default function twoSum(nums: number[], target: number): number[] {
-//   let hashmap: {};
-
-//   for (let index = 0; index < nums.length; index++) {
-//     const element = nums[index];
-//     for (let i = index+1; i < nums.length; i++) {
-//       if (hashmap[element+nums[1]]) {
-
-//       }
-//       const sum = element + nums[i];
-//       if (sum === target) {
-//         return [index, i]
-//       }
-//     }
-//   }
-// };
-
-
 // Hashmap Solution
-export default function twoSum(nums: number[], target: number): number[] | Error {
-	let hashMap = {};
-	
-	for(let index = 0; index < nums.length; index++) {
-		const number = nums[index];
+export default function twoSum(
+  nums: Array<number>,
+  target: number
+): number[] | Error {
+  let hashMap = {};
 
-		if(hashMap[target - number] !== undefined) {
-			return [hashMap[target - number], index];
-		}
-		hashMap[number] = index;
-	}
+  for (let index = 0; index < nums.length; index++) {
+    const number = nums[index];
 
-	return new Error("No two sum solution");
+    if (hashMap[target - number] !== undefined) {
+      return [hashMap[target - number], index];
+    }
+    hashMap[number] = index;
+  }
+
+  return new Error("No two sum solution");
 }
