4cc1d23262516eaae9399b1da460e8ed610c3f90
Kegbi <kegbi.networking@gmail.com>
2023-11-19 05:14:38 +0100

    Rewrote 198 problem solution to have constant space requirement, added comments

--- src/websites/leetcode/Medium/198_house_robber/house_robber.ts
+++ src/websites/leetcode/Medium/198_house_robber/house_robber.ts
@@ -2,13 +2,20 @@ export function rob(nums: number[]): number {
   if (nums.length === 0) return 0;
   if (nums.length === 1) return nums[0];
 
-  const dp: number[] = new Array(nums.length);
-  dp[0] = nums[0];
-  dp[1] = Math.max(nums[0], nums[1]);
+  // Initialize variables to store the max amount robbed up to the previous and current house
+  let maxRobbedUpToPrevious = nums[0];
+  let maxRobbedUpToCurrent = Math.max(nums[0], nums[1]);
 
   for (let i = 2; i < nums.length; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    // Calculate the new best value for the current house
+    const newBestValue = Math.max(
+      maxRobbedUpToCurrent,
+      maxRobbedUpToPrevious + nums[i],
+    );
+    maxRobbedUpToPrevious = maxRobbedUpToCurrent;
+    maxRobbedUpToCurrent = newBestValue;
   }
 
-  return dp[nums.length - 1];
+  // Return the maximum amount robbed
+  return maxRobbedUpToCurrent;
 }
