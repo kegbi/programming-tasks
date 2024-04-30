c0d338037f06b95e28a0365000b277fc7d7de273
Kegbi <kegbi.networking@gmail.com>
2024-04-30 18:19:22 +0200

    Added BinarySearch solution to 300 problem

--- src/websites/leetcode/Medium/300_longest_increasing_subsequence/longest_increasing_subsequence.ts
+++ src/websites/leetcode/Medium/300_longest_increasing_subsequence/longest_increasing_subsequence.ts
@@ -32,18 +32,55 @@
 // }
 
 // DP
+// export function lengthOfLIS(nums: number[]): number {
+//   if (nums.length === 0) return 0;
+//
+//   const lis = new Array(nums.length).fill(1);
+//
+//   for (let i = 1; i < nums.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//       if (nums[j] < nums[i]) {
+//         lis[i] = Math.max(lis[i], lis[j] + 1);
+//       }
+//     }
+//   }
+//
+//   return Math.max(...lis);
+// }
+
+// BinarySearch
 export function lengthOfLIS(nums: number[]): number {
   if (nums.length === 0) return 0;
 
-  const lis = new Array(nums.length).fill(1);
+  const finalSequence: number[] = [];
 
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[j] < nums[i]) {
-        lis[i] = Math.max(lis[i], lis[j] + 1);
+  function binarySearch(arr: number[], x: number): number {
+    let start = 0;
+    let end = arr.length - 1;
+    let resultIndex = -1;
+
+    while (start <= end) {
+      const mid = Math.floor((start + end) / 2);
+
+      if (arr[mid] < x) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+        resultIndex = mid;
       }
+    }
+
+    return resultIndex >= 0 ? resultIndex : start;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    const index = binarySearch(finalSequence, nums[i]);
+    if (index === finalSequence.length) {
+      finalSequence.push(nums[i]);
+    } else {
+      finalSequence[index] = nums[i];
     }
   }
 
-  return Math.max(...lis);
+  return finalSequence.length;
 }
